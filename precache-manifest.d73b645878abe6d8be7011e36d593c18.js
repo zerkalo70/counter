2a1cd893fd3c070736fa3f5a2ce04ab6bdf7ed09
Сергей <halygali@mail.ru>
2020-05-21 07:52:41 +0300

    Updates

---
+++ precache-manifest.d73b645878abe6d8be7011e36d593c18.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c337a2a61422c637048c6d1b393a2ac",
+    "url": "/counter/index.html"
+  },
+  {
+    "revision": "9c5ef2f9c3e3ec7197a5",
+    "url": "/counter/static/css/main.74fda56c.chunk.css"
+  },
+  {
+    "revision": "fce0c85491fd2009fbc3",
+    "url": "/counter/static/js/2.00d04896.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/counter/static/js/2.00d04896.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c5ef2f9c3e3ec7197a5",
+    "url": "/counter/static/js/main.dbf9d939.chunk.js"
+  },
+  {
+    "revision": "890e0606e2c9e34496c7",
+    "url": "/counter/static/js/runtime-main.01fdcc42.js"
+  }
+]);
